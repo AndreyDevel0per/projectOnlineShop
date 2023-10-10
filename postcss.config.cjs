a69abdfd650b7834921bf75690f2309808b51305
AndreyDevel0per <pimonov2003@gmail.com>
2023-10-10 15:58:05 +0500

    add postcssConfig

---
+++ postcss.config.cjs
@@ -0,0 +1,5 @@
+const nested = require("postcss-nested")
+const postcssPresetEnv = require("postcss-preset-env")
+const postcssCustomMedia = require("postcss-custom-media")
+
+module.exports = { plugins: [nested(), postcssPresetEnv(), postcssCustomMedia({})] }
