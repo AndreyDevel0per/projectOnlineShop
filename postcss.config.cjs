a775966dce68cd7eb28d092dc8f5eb8623812bc0
AndreyDevel0per <105583003+AndreyDevel0per@users.noreply.github.com>
2023-10-12 14:34:38 +0500

    Delete postcss.config.cjs

--- postcss.config.cjs
+++
@@ -1,7 +0,0 @@
-const nested = require('postcss-nested')
-const postcssPresetEnv = require('postcss-preset-env')
-const postcssCustomMedia = require('postcss-custom-media')
-
-module.exports = {
-  plugins: [nested(), postcssPresetEnv(), postcssCustomMedia({})]
-}
